682af4b19b991f40bc928887ecf2ff3a8f8a040c
pengwenwu <pengwenwu@corp-ci.com>
2019-10-27 23:51:56 +0800

    add

---
+++ RabbitMq/examples/send.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->queue_declare('hello', false, false, false, false);
+$msg = new AMQPMessage('Hello World!');
+$channel->basic_publish($msg, '', 'hello');
+echo " [x] Sent 'Hello World!'\n";
