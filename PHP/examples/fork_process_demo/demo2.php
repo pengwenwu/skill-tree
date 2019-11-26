e6284aaf996ecb951ff72da433fc4da69f60f7f6
pengwenwu <pww932589183@163.com>
2019-11-26 23:06:07 +0800

    add:php-demo

---
+++ PHP/examples/fork_process_demo/demo2.php
@@ -0,0 +1,17 @@
+<?php
+// 7次
+//for ($i = 1; $i <= 3; $i++) {
+//    $i_pid = pcntl_fork();
+//    if (0 == $i_pid) {
+//        echo "@子进程" . PHP_EOL;
+//    }
+//}
+
+// 3次
+for ( $i = 1; $i <= 3; $i++ ) {
+    $i_pid = pcntl_fork();
+    if ( 0 == $i_pid ) {
+        echo "@子进程".PHP_EOL;
+        exit;
+    }
+}
